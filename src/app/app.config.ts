46c96d4d99db1442d4bc31d33b648a19b005ea9c
Tshepiso28 <tshepisostudent@gmail.com>
2025-04-23 10:22:01 +0200

    error in app.config file

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,19 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection, inject } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { routes } from './app.routes';
+import { AuthService } from './services/auth.service';
+import { AuthInterceptor } from './interceptors/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes),
+    provideHttpClient(
+      withInterceptors([
+        (req, next) => {
+          const authInterceptor = new AuthInterceptor(inject(AuthService));
+          return authInterceptor.intercept(req, next);
+        }
+      ])
+    )
+  ]
 };
